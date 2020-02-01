3a7635ff1d7a032a7dbe18665d51d5d4b28c88e1
Krystian <20971818+Modredo@users.noreply.github.com>
2020-02-01 15:26:12 +0000

    Added resizing

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3 
 import os, rawpy, imageio
+from PIL import Image
+from resizeimage import resizeimage
 
 #TODO add argparse to input RAW file exttension 
 #   or list common format for autodetection 
 
-def main()
-    images = listdir()
+def main():
+    images = os.listdir()
     for image in images:
-    if 'NEF' in image:
-        print(raw_to_jpg(image))
+        if 'NEF' in image:
+            print(raw_to_jpg(image))
+
+    new_images = os.listdir()
+    for image in new_images:
+        if 'jpg' in image:
+            print(resize(image))
 
 def raw_to_jpg(image):
     try:
@@ -19,6 +26,15 @@ def raw_to_jpg(image):
         return f'image converted: {image}'
     except:
         return 'image conversion failed'
+
+
+def resize(path):
+    with open(path, 'r+b') as f:
+        with Image.open(f) as image:
+            cover = resizeimage.resize_cover(image, [1206, 802])
+            cover.save('r'+path, image.format)
+    return 'image resized.'
+
 
 if "__name__" == "__main__":
     main()
